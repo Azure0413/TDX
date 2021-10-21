98ecdab7ae3f659a0f4a1d6bd3786e5fc02c9cbb
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2021-10-21 16:42:48 +0800

    Add files via upload

--- TDX/auth.py
+++ TDX/auth.py
@@ -7,8 +7,8 @@ import base64
 from requests import request
 from pprint import pprint
 
-app_id = 'YOUR APP ID(L1)'
-app_key = 'YOUR APP KEY(L1)'
+app_id = '6e4483c0931943f0b52fdf61fa97277b'
+app_key = 'UjB5oLj7-KT1W6Ki1NtC1OoYLo8'
 
 class Auth():
 
@@ -17,22 +17,22 @@ class Auth():
         self.app_key = app_key
 
     def get_auth_header(self):
-        xdate = 'Wed, 20, Oct, 2021, 13:00:45 GMT'
+        xdate = 'x-date: Thu, 21 Oct 2021 07:48:02 GMT'
         hashed = hmac.new(self.app_key.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
         signature = base64.b64encode(hashed.digest()).decode()
 
-        authorization = hmac username="FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
+        authorization = 'hmac username=FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF' + self.app_id + '", ' + \
                         'algorithm="hmac-sha1", ' + \
                         'headers="x-date", ' + \
-                        'signature=FPHnUuYn+TszUXXStubpJPs87LE=' + signature + '"'
+                        'signature=EKw7drl2J+4s4/TuTnk+9daZHnM=' + signature + '"'
         return {
             'Authorization': authorization,
-            'x-date': xdate,
+            'x-date': format_date_time(mktime(datetime.now().timetuple())),
             'Accept - Encoding': 'gzip'
         }
 
 
 if __name__ == '__main__':
     a = Auth(app_id, app_key)
-    response = request('get', 'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei?$top=30&$format=JSON', headers= a.get_auth_header())
+    response = request('get', 'https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?$top=30&$format=JSON', headers= a.get_auth_header())
     pprint(response.content)
